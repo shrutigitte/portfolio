db9523544ed75af113df07a9e15cd9a7e2c9d993
Shruti Gitte <shrutigitte@gmail.com>
2024-08-14 20:31:06 +0530

    Connected href:tel: in phone

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -8,12 +8,11 @@ const Contacts = () => {
       className="text-4xl lg:text-5xl text-purple-700 text-center my-20">Get in Touch
       </motion.h2>
 
-      <div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1.5}}
-      className="tracking-tighter text-center">
-        <motion.p whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1.5}}
-        className="my-4 text-fuchsia-600 font-medium"> <span className="text-blue-500 font-normal">MobileNo:-</span> {CONTACT.phoneNo}
-        </motion.p>
-        <a href="mailto:shrutigitte@gmail.com" target="_blank" className="text-fuchsia-600 font-medium border-b border-fuchsia-600"> <span className="font-normal text-blue-500 border-none" >email:-</span> {CONTACT.email}</a>
+      <div className="text-center">
+        <a href="tel:+918506022170" 
+        className="my-4 text-fuchsia-600 font-medium"> <p className="text-blue-500 font-normal">Mobile:-</p> {CONTACT.phoneNo}
+        </a> <br />
+        <a href="mailto:shrutigitte@gmail.com" target="_blank" className="text-fuchsia-600 font-medium border-b border-fuchsia-600"> <span className=" text-blue-500 " >email:-</span> {CONTACT.email}</a>
       </div>
     </div>
   );
